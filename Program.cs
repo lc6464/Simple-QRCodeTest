5ae686f2403307de759462b6adbb773b2d5baac0
lc6464 <64722907+lc6464@users.noreply.github.com>
2022-06-17 23:28:50 +0800

    测试用代码

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+/*
+Bitmap bitmap = new(@"Rendered.png");
+
+BitmapLuminanceSource luminanceSource = new(bitmap);
+HybridBinarizer binarizer = new(luminanceSource);
+BinaryBitmap binaryBitmap = new(binarizer);
+
+QRCodeReader reader = new();
+Result result = reader.decode(binaryBitmap);
+
+if (result == null) {
+	Console.WriteLine("识别失败。");
+} else {
+	Console.WriteLine("识别成功！");
+	File.WriteAllText(@"data.txt", result.Text);
+}
+*/
+
+
+
+Dictionary<EncodeHintType, object> hints = new () {
+	{ EncodeHintType.ERROR_CORRECTION, ZXing.QrCode.Internal.ErrorCorrectionLevel.L }
+};
+
+QRCodeWriter writer = new ();
+BitMatrix data = writer.encode("", BarcodeFormat.QR_CODE, 400, 400, hints);
+
+
+BitmapRenderer renderer = new ();
+Bitmap bitmap = renderer.Render(data, BarcodeFormat.QR_CODE, "");
+bitmap.Save(@"Rendered.png");
+
+
+
+bitmap.Dispose();
---
+++ README.md
@@ -0,0 +1,6 @@
+## 简单的 QR 码测试
+> 使用 [ZXing.Net](https://github.com/micjahn/ZXing.Net)
+> ZXing.Net 使用 [Apache License 2.0](https://github.com/micjahn/ZXing.Net/blob/master/COPYING)
+
+直接拿来用显然不现实，<br/>
+我其实是拿来配合我的手表的核酸码展示器使用的。
