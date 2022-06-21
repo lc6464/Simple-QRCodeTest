2371bc30f836100638e4b0d62a21454825cb99db
lc6464 <64722907+lc6464@users.noreply.github.com>
2022-06-21 16:32:55 +0800

    v1.0.0.0 可以用了

--- Program.cs
+++ Program.cs
@@ -1,39 +1,164 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
-
-/*
-Bitmap bitmap = new(@"Rendered.png");
-
-BitmapLuminanceSource luminanceSource = new(bitmap);
-HybridBinarizer binarizer = new(luminanceSource);
-BinaryBitmap binaryBitmap = new(binarizer);
-
-QRCodeReader reader = new();
-Result result = reader.decode(binaryBitmap);
-
-if (result == null) {
-	Console.WriteLine("识别失败。");
-} else {
-	Console.WriteLine("识别成功！");
-	File.WriteAllText(@"data.txt", result.Text);
+ActionType type;
+string[]? strings = null;
+if (args.Length > 1) { // 直接读取 CLI 参数
+	type = ParseActionType(args[0]);
+	if (type == ActionType.Unknow) {
+		Console.Error.WriteLine("您输入的操作类型有误！");
+		return;
+	}
+	strings = args[1..];
+} else if (args.Length == 1) { // 只传入一个参数，当作数据处理，自动识别操作
+	type = ParseActionType(args[0]);
+	if (type == ActionType.Unknow) {
+		if (File.Exists(args[0])) { // 文件存在，当作解码
+			Console.WriteLine("您未传入操作类型，但传入了一个参数，且检测到此文件存在，已进入解码逻辑。");
+			type = ActionType.Decode;
+			strings = new[] { args[0] };
+		} else { // 文件不存在，当作编码
+			Console.WriteLine("您未传入操作类型，但传入了一个参数，且检测到此文件不存在，已进入编码逻辑。");
+			type = ActionType.Encode;
+			strings = new[] { args[0] };
+		}
+	}
+} else { // 什么也没有传入
+	Console.Write("请输入操作类型：");
+	type = ParseActionType(Console.ReadLine());
+	if (type == ActionType.Unknow) {
+		Console.Error.WriteLine("您输入的操作类型有误！");
+		return;
+	}
 }
-*/
+
+List<string> readList = new();
+if (strings == null || strings.Length == 0) {
+	Console.WriteLine($"请输入要{(type == ActionType.Encode ? "编码的文本" : "解码的图片路径")}，一行一个：");
+	while (true) {
+		string? read = Console.ReadLine()?.Trim();
+		if (string.IsNullOrWhiteSpace(read)) {
+			break;
+		}
+		readList.Add(read);
+	}
+	strings = readList.ToArray();
+	readList.Clear();
+}
+
+if (strings.Length == 0) {
+	Console.WriteLine($"没有要{(type == ActionType.Encode ? "编码的文本" : "解码的图片路径")}！");
+	return;
+}
 
 
+DirectoryInfo directoryInfo = new("Results"); // 创建文件夹
+if (!directoryInfo.Exists) {
+	if (File.Exists(directoryInfo.Name)) {
+		Console.Error.WriteLine($"存在 {directoryInfo.Name} 文件，无法创建文件夹！");
+		return;
+	}
+	try {
+		directoryInfo.Create();
+	} catch (Exception e) {
+		Console.Error.WriteLine($"创建文件夹 {directoryInfo.Name} 时发生异常！");
+		Console.Error.Write($"异常：{e.Message}\r\n详细信息：");
+		Console.Error.WriteLine(e);
+		return;
+	}
+}
 
-Dictionary<EncodeHintType, object> hints = new () {
+Dictionary<EncodeHintType, object> hints = new() { // 纠错等级：低
 	{ EncodeHintType.ERROR_CORRECTION, ZXing.QrCode.Internal.ErrorCorrectionLevel.L }
 };
 
-QRCodeWriter writer = new ();
-BitMatrix data = writer.encode("", BarcodeFormat.QR_CODE, 400, 400, hints);
+
+if (type == ActionType.Encode) { // 编码模式
+	foreach (string str in strings) {
+		Console.Write($"{(str.Length < 32 ? str : (str[..32] + "..."))} ... ");
+
+		try { // 编码数据
+			QRCodeWriter writer = new();
+			BitMatrix data = writer.encode(str, BarcodeFormat.QR_CODE, 1000, 1000, hints);
+
+			try { // 生成二维码
+				BitmapRenderer renderer = new();
+				using Bitmap bitmap = renderer.Render(data, BarcodeFormat.QR_CODE, str);
+
+				Guid guid = Guid.NewGuid();
+				string fileName = $@"Results\{DateTime.Now:HH-mm-ss}_{guid}.png";
+
+				try {
+					bitmap.Save(fileName);
+					Console.WriteLine(fileName);
+				} catch (Exception e) {
+					Console.Error.WriteLine($"写入文件 {fileName} 时发生异常！");
+					Console.Error.Write($"异常：{e.Message}\r\n详细信息：");
+					Console.Error.WriteLine(e);
+				}
+			} catch (Exception e) {
+				Console.Error.WriteLine($"生成二维码时发生异常！");
+				Console.Error.Write($"异常：{e.Message}\r\n详细信息：");
+				Console.Error.WriteLine(e);
+			}
+		} catch (Exception e) {
+			Console.Error.WriteLine($"编码数据时发生异常！");
+			Console.Error.Write($"异常：{e.Message}\r\n详细信息：");
+			Console.Error.WriteLine(e);
+		}
+	}
+} else { // 解码模式
+	foreach (string str in strings) {
+		Console.Write($"{str} ... ");
+
+		try { // 读取文件
+			if (!File.Exists(str)) {
+				Console.Error.WriteLine($"文件不存在！");
+				continue;
+            }
+
+			using Bitmap bitmap = new(str);
+
+			try {
+				QRCodeReader reader = new();
+				Result result = reader.decode(new BinaryBitmap(new HybridBinarizer(new BitmapLuminanceSource(bitmap))));
+
+				if (result == null) {
+					Console.Error.WriteLine("二维码识别失败。");
+				} else {
+					Guid guid = Guid.NewGuid();
+					string fileName = $@"Results\{DateTime.Now:HH-mm-ss}_{guid}.txt";
+
+					try {
+						File.WriteAllText(fileName, $"{str}\r\n{result.Text}");
+						Console.WriteLine(fileName);
+					} catch (Exception e) {
+						Console.Error.WriteLine($"写入文件 {fileName} 时发生异常！");
+						Console.Error.Write($"异常：{e.Message}\r\n详细信息：");
+						Console.Error.WriteLine(e);
+					}
+				}
+			} catch (Exception e) {
+				Console.Error.WriteLine($"识别二维码时发生异常！");
+				Console.Error.Write($"异常：{e.Message}\r\n详细信息：");
+				Console.Error.WriteLine(e);
+			}
+		} catch (Exception e) {
+			Console.Error.WriteLine($"读取文件时发生异常！");
+			Console.Error.Write($"异常：{e.Message}\r\n详细信息：");
+			Console.Error.WriteLine(e);
+		}
+	}
+}
 
 
-BitmapRenderer renderer = new ();
-Bitmap bitmap = renderer.Render(data, BarcodeFormat.QR_CODE, "");
-bitmap.Save(@"Rendered.png");
 
+static ActionType ParseActionType(string? s, bool toLower = true) {
+	return (toLower ? s?.ToLower() : s) switch {
+		"d" or "decode" or "Decode" => ActionType.Decode,
+		"e" or "encode" or "Encode" => ActionType.Encode,
+		_ => ActionType.Unknow
+	};
+}
 
-
-bitmap.Dispose();
+enum ActionType { Encode, Decode, Unknow }
--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 > 使用 [ZXing.Net](https://github.com/micjahn/ZXing.Net)<br/>
 > ZXing.Net 使用 [Apache License 2.0](https://github.com/micjahn/ZXing.Net/blob/master/COPYING)
 
-直接拿来用显然不现实，<br/>
-我其实是拿来配合我的手表的核酸码展示器使用的。
+现在已经可以拿来用了。
+
+目前仅支持 Windows 系统。
